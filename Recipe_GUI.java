041e7d2f0b6055a1c43d1f81a9bc46e45b21da1d
Geotiger123 <82854175+Geotiger123@users.noreply.github.com>
2021-04-23 19:59:12 -0700

    Recipe_GUI_v0.0.2
    Bug fixes:
    *In getRecipe method recipeID int array was a out of bounds, went from 1-3 instead of 0-2. line 75,78,81
    Changes:
    *In checkIngredient method, added hashmap to store ingredient list to simplify the for loop. line 32-46, 48-52
    *In checkIngredient method, added feature to removed duplicate and unlisted ingredients from ingredientInt  Array.  line 30, 48-59

--- Recipe_GUI.java
+++ Recipe_GUI.java
@@ -21,68 +21,47 @@ public class Recipe_GUI {
    }
    
    //method for comma seperating ingredients, and converting into a list of predefined integers
-   public static int[] checkIngredient(String str) {
-   
+   public static int[] checkIngredient(String str) {   
       
-      str = str.toLowerCase();
-      str = str.replaceAll("\\s+","");
-      
-      String ingredientList[] = str.split(",");
-      
-      int ingredientInt[] = new int[ingredientList.length];
-      
-      for (int i = 0; i<ingredientList.length; i++) {
-         if (ingredientList[i].equals("milk")) {
-            ingredientInt[i] = 1;
-         }
-         else if(ingredientList[i].equals("pasta")) {
-            ingredientInt[i] = 2;
-         }
-         else if(ingredientList[i].equals("cheese")) {
-            ingredientInt[i] = 3;
-         }
-         else if(ingredientList[i].equals("oil")) {
-            ingredientInt[i] = 4;
-         }
-         else if(ingredientList[i].equals("chocolatechips")) {
-            ingredientInt[i] = 5;
-         }
-         else if(ingredientList[i].equals("flour")) {
-            ingredientInt[i] = 6;
-         }
-         else if(ingredientList[i].equals("sugar")) {
-            ingredientInt[i] = 7;
-         }
-         else if(ingredientList[i].equals("nuts")) {
-            ingredientInt[i] = 8;
-         }
-         else if(ingredientList[i].equals("vanilla")) {
-            ingredientInt[i] = 9;
-         }
-         else if(ingredientList[i].equals("salt")) {
-            ingredientInt[i] = 10;
-         }
-         else if(ingredientList[i].equals("pepper")) {
-            ingredientInt[i] = 11;
-         }
-         else if(ingredientList[i].equals("chicken")) {
-            ingredientInt[i] = 12;
-         }
-         else if(ingredientList[i].equals("water")) {
-            ingredientInt[i] = 13;
-         }
-         else if(ingredientList[i].equals("rice")) {
-            ingredientInt[i] = 14;
-         }
-         else {
-            System.out.println("Ingredient "+ingredientList[i]+" not found.");
-         }
-         
-      }
-   
-      System.out.println("Ingredient conversion and check complete");
-   
-      return ingredientInt;
+        str = str.toLowerCase();
+        str = str.replaceAll("\\s+","");
+
+        String ingredientList[] = str.split(",");
+        LinkedHashSet<Integer> removeDup= new LinkedHashSet<Integer>(); //remove duplicates and ingredient not on database
+        
+        HashMap<String,Integer> ingredientI = new HashMap<String, Integer>();// changed to hashmap
+        ingredientI.put("milk",1);
+        ingredientI.put("pasta",2);
+        ingredientI.put("cheese",3);
+        ingredientI.put("oil",4);
+        ingredientI.put("chocolatechips",5);
+        ingredientI.put("flour",6);
+        ingredientI.put("sugar",7);
+        ingredientI.put("nuts",8);
+        ingredientI.put("vanilla",9);
+        ingredientI.put("salt",10);
+        ingredientI.put("pepper",11);
+        ingredientI.put("chicken",12);
+        ingredientI.put("water",13);
+        ingredientI.put("rice",14);
+
+        for (int i = 0; i<ingredientList.length; i++) { //checks if ingredient is in database
+            if(ingredientI.containsKey(ingredientList[i])) {
+                removeDup.add(ingredientI.get(ingredientList[i]));//adds to hashset which removes duplicates 
+            }
+            else System.out.println("Ingredient "+ingredientList[i]+" not found.");//in cases where not found
+
+        }
+        int ingredientInt[]= new int[removeDup.size()];
+        int index=0;
+        for(Integer i : removeDup ) { // used to add the hashset values into int array.
+            ingredientInt[index++] = i;
+        }
+
+
+        System.out.println("Ingredient conversion and check complete");
+
+        return ingredientInt;
    }
    
    //method for determining the recipe(s) to display
@@ -93,13 +72,13 @@ public class Recipe_GUI {
    
       for (int i = 0; i<ingredientInt.length; i++) {
          if (ingredientInt[i] == 1 || ingredientInt[i] == 2 || ingredientInt[i] == 3 || ingredientInt[i] == 4) {
-               recipeID[1] = 1;
+               recipeID[0] = 1; // George - caught a small bug, recipe is going out of bound should be 0-2 instead of 1-3 for arrays.
          }
          else if (ingredientInt[i] == 5 || ingredientInt[i] == 6 || ingredientInt[i] == 7 || ingredientInt[i] == 8 || ingredientInt[i] == 9) {
-               recipeID[2] = 1;
+               recipeID[1] = 1;
          }
          else if (ingredientInt[i] == 10 || ingredientInt[i] == 11 || ingredientInt[i] == 12 || ingredientInt[i] == 13 || ingredientInt[i] == 14) {
-               recipeID[3] = 1;
+               recipeID[2] = 1;
          }
          else {
             System.out.println("No compatible recipes found for "+ingredientInt[i]+".");
