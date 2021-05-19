a5e7287ec80719a74c7c181e515798b1292c6426
rhawkens99 <82605139+rhawkens99@users.noreply.github.com>
2021-05-19 12:18:28 -0700

    SampleQueries.js update
    Ability to add a recipe with ingredients to firebase and everything is placed correctly

--- React/SampleQueries.js
+++ React/SampleQueries.js
@@ -30,24 +30,38 @@ require("firebase/firestore");
 
 var db = firebase.firestore();
 
-var recipesRef = db.collection("recipes");
-var ingredientsRef = db.collection("ingredients");
-var RIref = db.collection("RI");
-var usersRef = db.collection("users");
-
-function setRecipe(recipeName) {
-  recipesRef.doc("Recipe2").set({
-    name: "Recipe 4", recipeId: "3"
-  });
-}
-
-
 async function getUserId() {
   // user 1 or 2's recipe
   var uId = Math.floor(Math.random() * 2) + 1;
 
-  return db.collection('users').where('userId', '==', uId).get().then(snapshot => {
+  var name;
+  if (uId === 1) {
+    name = 'testUser';
+  }
+  else {
+    name = 'testUser2';
+  }
+
+  return db.collection('users').where('userName', '==', name).get().then(snapshot => {
     return snapshot.docs.map(doc => doc.id);
+  })
+}
+
+async function getRecipeId(recipeName) {
+  return db.collection('recipes').where('recipeName', '==', recipeName.toLowerCase()).get().then(snapshot => {
+    return snapshot.docs.map(doc => doc.id);
+  })
+}
+
+async function getIngId(ingredientName) {
+  return db.collection('ingredients').where('ingredientName', '==', ingredientName.toLowerCase()).get().then(snapshot => {
+    return snapshot.docs.map(doc => doc.id);
+  })
+}
+
+async function checkIfIngredientExists(name) {
+  return db.collection('ingredients').where('ingredientName', '==', name.toLowerCase()).get().then(snapshot => {
+    return snapshot.docs.map(doc => doc.exists);
   })
 }
 
@@ -58,29 +72,68 @@ async function addRecipe(recipeName, ingredientList) {
   iList = iList.concat(str);
 
   // add recipe to database with correct user id, ingredient list, and place everything correctly into RI
+  // also find max id? maybe don't use pre-generated id's and instead create them myself
 
   // get userId
   var arr = await getUserId();
   var custUId = arr[0];
-  console.log(custUId);
 
   // add recipe
   db.collection("recipes").add({
-    recipeName: recipeName,
-    recipeId: 3,
+    recipeName: recipeName.toLowerCase(),
+    //recipeId: 3,
     userId: custUId
   });
 
-  // add ingredients to ingredients collection. Check for duplicates
+  console.log("Recipes Collection updated!");
 
+  // add iList to ingredients collection. Check for duplicates
+  for (var i = 0; i < iList.length; i++) {
+
+    var exists = await checkIfIngredientExists(iList[i]);
+
+    if (exists[0]) {
+      console.log("Ingredient already in database: " + iList[i]);
+    }
+
+    else {
+      db.collection("ingredients").add({
+        ingredientName: iList[i].toLowerCase(),
+        //ingredientId: 3,
+      });
+    }
+  }
+
+  console.log("Ingredients Collection Updated");
+
+  // add everything to RI table
+
+  // loop through iList for names.
+  // find docId of both recipe and ingredients
+  var tId = await getRecipeId(recipeName);
+  var recId = tId[0];
+
+  for (i = 0; i < iList.length; i++) {
+    tId = await getIngId(iList[i]);
+    var ingId = tId[0];
+
+    db.collection('RI').add({
+      ingredientName: iList[i].toLowerCase(),
+      ingredientId: ingId,
+      recipeId: recId
+    });
+  }
+
+  console.log("RI Collections Upgraded");
 
 }
+
 
 
 //const auth = firebase.auth();
 //const analytics = firebase.analytics();
 
-class App extends React.Component {
+class SampleQueries extends React.Component {
 
   constructor(props) {
     super(props);
@@ -137,4 +190,4 @@ class App extends React.Component {
 
 
 
-export default App;
+export default SampleQueries;
