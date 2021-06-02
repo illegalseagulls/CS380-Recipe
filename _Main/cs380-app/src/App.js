db93c75b8682565d5c0daeb272ae59ddbbb6662e
CaptainRed <sdavis9428@gmail.com>
2021-06-02 11:56:34 -0700

    links and style update
    added links/routes; set up basic structuring system for recipe search page

--- _Main/cs380-app/src/App.js
+++ _Main/cs380-app/src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 import RecipeSearch from './components/recipeSearch';
 import RecipeList from './components/recipelist';
+import AddRecipe from './components/addRecipe'
 
 class App extends React.Component {
 
@@ -25,11 +27,30 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Router>
+        <nav>
+          <ul>
+            <li className="leftItems"><a href="/">Search Recipe</a></li>
+            <li className="leftItems"><a href="/components/addRecipe">Add Recipe</a></li>
+            <li className="leftItems"><a href="#">My Recipes</a></li>
+            <li className="leftItems"><a href="#">Profile</a></li>
+          </ul>
+        </nav>
+        <Route exact path="/" render={props => (
+          <div>
           <h1 className="App-h1">Recipe Finder</h1>
-          <RecipeSearch recipeSearch={this.recipeSearch}/>
-          <RecipeList recipes={this.state.recipes} />
-        </header>
+          <header className="App-recipesearch">
+            <section className="topleft">
+              <RecipeSearch recipeSearch={this.recipeSearch}/>
+            </section>
+            <section className="right">
+              <RecipeList recipes={this.state.recipes} />
+            </section>
+          </header>
+          </div>
+        )}/>
+        <Route path="/components/addRecipe" component={AddRecipe}/>
+        </Router>
       </div>
     );
   }
