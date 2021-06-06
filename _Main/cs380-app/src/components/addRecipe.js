169166a40f6a1c9037372dba303ffcd4b877afcb
Richard <82605139+rhawkens99@users.noreply.github.com>
2021-06-05 20:03:02 -0700

    Cleaning up code and started styling

--- _Main/cs380-app/src/components/addRecipe.js
+++ _Main/cs380-app/src/components/addRecipe.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { addRecipe } from './Queries';
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-
+import './addRecipe.css'
+import { TextField, Button, Select, MenuItem, FormControl, InputLabel, IconButton } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 class AddRecipe extends React.Component {
 
@@ -108,94 +103,158 @@ class AddRecipe extends React.Component {
         this.setState({ directionDisplay: '', recipeDisplayName: '', ingredientList: [], databaseDirections: '', databaseIngredientList: [], });
     }
 
+    submitButton = {
+        backgroundColor: '#1B2845',
+        color: 'white',
+        marginLeft: 20,
+        top: 5
+    };
+
+    submitButton2 = {
+        backgroundColor: '#1B2845',
+        color: 'white',
+        marginLeft: 150,
+        marginTop: 5
+    };
+
+    submitButton3 = {
+        backgroundColor: '#1B2845',
+        color: 'white',
+        marginLeft: 50,
+    };
+
+
     render() {
         return (
-            <React.Fragment>
+            <div className='wrapper'>
                 <h1>Add Recipe</h1>
-                <div style={{ position: 'fixed', left: 100, padding: 0, margin: 0, overflowY: 'scroll', width: '100%' }}>
-                    <div style={{ display: 'inline-block', float: 'left', marginRight: 300 }}>
-                        {/* Recipe Name Add */}
-                        <h2>Add Recipe Name</h2>
-                        <div style={{ marginBottom: 30, /*borderStyle: 'solid', borderColor: 'black', borderWidth: 1,*/ height: 70, width: 500 }}>
-                            <form style={{ marginTop: 5 }} onSubmit={this.handleRecipeSubmit}>
-                                <TextField id='recipeName' label='Recipe Name' variant='outlined' onChange={this.handleRecipeNameChange} autoComplete='off' />
-                                <Button type='submit' value='Submit' style={{ marginLeft: 20, top: 5 }} variant='contained' > Submit </Button>
-                            </form>
-                        </div>
-
-                        {/* Ingredient Add */}
-                        <h2>Input Ingredients</h2>
-                        <div style={{ marginBottom: 30, /*borderStyle: 'solid', borderColor: 'black', borderWidth: 1,*/ height: 100, marginBottom: 50 }}>
-                            <form style={{ marginTop: 20 }} onSubmit={this.handleIngredientSubmit} >
-                                <div>
-                                    <TextField
-                                        id='ingredientName'
-                                        label='Ingredient Name'
-                                        variant='outlined'
-                                        onChange={this.handleIngredientNameChange} autoComplete='off'
-                                        style={{ marginBottom: 10 }}
-                                    />
-                                </div>
-                                <div>
-                                    <TextField id='amount' label='Amount of Ingredient' variant='outlined' onChange={this.handleAmountChange} autoComplete='off' />
-                                    <FormControl variant='outlined' >
-                                        <InputLabel id='unitLabel'>Unit</InputLabel>
-                                        <Select
-                                            labelId='unitLabel'
-                                            id='unitSelector'
-                                            value={this.state.unit ? this.state.unit : 'N/A'}
-                                            onChange={this.handleUnitChange}
-                                            label='Unit'
-                                            style={{ marginLeft: 5, width: 100 }}
-                                        >
-                                            <MenuItem value={'N/A'}>N/A</MenuItem>
-                                            <MenuItem value={'tsp'}>tsp</MenuItem>
-                                            <MenuItem value={'Tbsp'}>Tbsp</MenuItem>
-                                            <MenuItem value={'cup'}>cup</MenuItem>
-                                            <MenuItem value={'oz'}>oz</MenuItem>
-                                            <MenuItem value={'lb'}>lb</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                    <Button type='submit' value='Add Ingredient' variant='contained' style={{ marginLeft: 20, top: 10 }}>Add Ingredient</Button>
-                                </div>
-                            </form>
-                        </div>
-
-                        {/* Directions Add */}
-                        <h2>Input Directions</h2>
-                        <div style={{ marginBottom: 50, /*borderStyle: 'solid', borderColor: 'black', borderWidth: 1,*/ height: 350 }}>
-                            <form style={{ marginTop: 20, marginLeft: 5, marginRight: 5 }} >
-                                <div>
-                                    <textarea style={{ height: 275, width: 490 }} onChange={this.handleDirectionsChange} value={this.state.directions} />
-                                </div>
-                                <Button style={{ marginLeft: 150, marginTop: 5 }} onClick={this.handleDirectionsSubmit} variant='contained' >Submit Directions</Button>
-                            </form>
-                        </div>
+                <div className='addWrapper'>
+                    {/* Recipe Name Add */}
+                    <h2>Add Recipe Name</h2>
+                    <div className='recipeName'>
+                        <form
+                            style={{ marginTop: 5 }}
+                            onSubmit={this.handleRecipeSubmit}
+                        >
+                            <TextField
+                                id='recipeName'
+                                label='Recipe Name'
+                                variant='outlined'
+                                onChange={this.handleRecipeNameChange}
+                                autoComplete='off'
+                            />
+                            <Button
+                                type='submit'
+                                value='Submit'
+                                style={this.submitButton}
+                                variant='contained'
+                            > Submit </Button>
+                        </form>
                     </div>
 
-                    {/* Display Recipe */}
-                    <div style={{ display: 'inline-block', float: 'left', }}>
-                        <h2>Display of Recipe
-                            <Button style={{ marginLeft: 50 }} onClick={this.handleDisplaySubmit} variant='contained'>Save Recipe</Button>
-                        </h2>
-                        <div style={{ borderStyle: 'solid', borderColor: 'black', borderWidth: 1, width: 600, height: 700 }}>
-                            <h2>Recipe Name: {this.state.recipeDisplayName}</h2>
-                            <h2>Ingredient List:</h2>
-                            {this.state.ingredientList.map((ingredient) => (
-                                <div key={ingredient}>
-                                    <li key={ingredient} style={{ marginLeft: 10 }}>{ingredient}
-                                        <button key={ingredient} style={{ marginLeft: 50 }} onClick={() => this.removeIngredient({ ingredient })}>Remove</button>
-                                    </li>
-                                </div>
-                            ))}
-                            <h2>Directions:</h2>
-                            <p style={{ whiteSpace: 'pre-wrap' }}>
-                                {this.state.directionDisplay}
-                            </p>
-                        </div>
+                    {/* Ingredient Add */}
+                    <h2>Input Ingredients</h2>
+                    <div className='ingredientAdd'>
+                        <form
+                            style={{ marginTop: 20 }}
+                            onSubmit={this.handleIngredientSubmit}
+                        >
+                            <div>
+                                <TextField
+                                    id='ingredientName'
+                                    label='Ingredient Name'
+                                    variant='outlined'
+                                    onChange={this.handleIngredientNameChange} autoComplete='off'
+                                    style={{ marginBottom: 10 }}
+                                />
+                            </div>
+                            <div>
+                                <TextField
+                                    id='amount'
+                                    label='Amount of Ingredient'
+                                    variant='outlined'
+                                    onChange={this.handleAmountChange}
+                                    autoComplete='off'
+                                />
+                                <FormControl variant='outlined' >
+                                    <InputLabel id='unitLabel'>Unit</InputLabel>
+                                    <Select
+                                        labelId='unitLabel'
+                                        id='unitSelector'
+                                        value={this.state.unit ? this.state.unit : 'N/A'}
+                                        onChange={this.handleUnitChange}
+                                        label='Unit'
+                                        style={{ marginLeft: 5, width: 100 }}
+                                    >
+                                        <MenuItem value={'N/A'}>N/A</MenuItem>
+                                        <MenuItem value={'tsp'}>tsp</MenuItem>
+                                        <MenuItem value={'Tbsp'}>Tbsp</MenuItem>
+                                        <MenuItem value={'cup'}>cup</MenuItem>
+                                        <MenuItem value={'oz'}>oz</MenuItem>
+                                        <MenuItem value={'lb'}>lb</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <Button
+                                    type='submit'
+                                    value='Add Ingredient'
+                                    variant='contained'
+                                    style={this.submitButton}
+                                >Add Ingredient</Button>
+                            </div>
+                        </form>
+                    </div>
+
+                    {/* Directions Add */}
+                    <h2>Input Directions</h2>
+                    <div className='addDirections'>
+                        <form
+                            style={{ marginTop: 20, marginLeft: 5, marginRight: 5 }}
+                        >
+                            <div>
+                                <textarea
+                                    className="customTextArea"
+                                    onChange={this.handleDirectionsChange} value={this.state.directions}
+                                />
+                            </div>
+                            <Button
+                                style={this.submitButton2}
+                                onClick={this.handleDirectionsSubmit}
+                                variant='contained'
+                            >Submit Directions</Button>
+                        </form>
                     </div>
                 </div>
-            </React.Fragment >
+
+                {/* Display Recipe */}
+                <div className='displayWrapper'>
+                    <h2>Display of Recipe
+                            <Button
+                            style={this.submitButton3}
+                            onClick={this.handleDisplaySubmit}
+                            variant='contained'
+                        >Save Recipe</Button>
+                    </h2>
+                    <div className='recipeDisplayBox'>
+                        <h2>Recipe Name: {this.state.recipeDisplayName}</h2>
+                        <h2>Ingredient List:</h2>
+                        {this.state.ingredientList.map((ingredient) => (
+                            <div key={ingredient}>
+                                <li key={ingredient} style={{ marginLeft: 10 }}>{ingredient}
+                                    <IconButton
+                                        key={ingredient}
+                                        style={{ marginLeft: 50, color: '#1B2845' }}
+                                        onClick={() => this.removeIngredient({ ingredient })}
+                                    ><DeleteIcon /></IconButton>
+                                </li>
+                            </div>
+                        ))}
+                        <h2>Directions:</h2>
+                        <p style={{ whiteSpace: 'pre-wrap' }}>
+                            {this.state.directionDisplay}
+                        </p>
+                    </div>
+                </div>
+            </div >
         );
     }
 }
